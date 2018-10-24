10499b5b558387f5550d4bda5796ac60c2c67804
Ricardo García Fernández <ricardogarfe@gmail.com>
2018-10-24 17:08:09 +0200

    Compilation error

--- src/test/java/com/myorg/MyServiceApplicationTests.java
+++ src/test/java/com/myorg/MyServiceApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import static org.junit.Assert.assertTrue
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = MyServiceApplication.class)
