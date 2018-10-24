d101c58a0395356c836f116e9a27d34dda9f4752
Ricardo García Fernández <ricardogarfe@gmail.com>
2018-10-24 17:04:37 +0200

    test to fail and check messages

--- src/test/java/com/myorg/MyServiceApplicationTests.java
+++ src/test/java/com/myorg/MyServiceApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import static org.junit.Assert.assertTrue
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = MyServiceApplication.class)
@@ -13,5 +14,10 @@ public class MyServiceApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+    
+    @Test
+    public void testToFail () {
+        assertTrue(false);
     }
 }
